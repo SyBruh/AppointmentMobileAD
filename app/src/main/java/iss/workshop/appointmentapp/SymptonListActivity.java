a665501fe2238a55d7f90d052193c760086fcc5f
watari1018 <watari1018@163.com>
2024-02-08 04:06:51 +0800

    Layout 1.0

---
+++ app/src/main/java/iss/workshop/appointmentapp/SymptonListActivity.java
@@ -0,0 +1,14 @@
+package iss.workshop.appointmentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SymptonListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sympton_list);
+    }
+}
